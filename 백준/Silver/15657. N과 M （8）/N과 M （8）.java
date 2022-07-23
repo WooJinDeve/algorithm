3c53e8f88498258de25987d8cdf9255ca530ff98
WooJinDeve <106054507+WooJinDeve@users.noreply.github.com>
2022-07-23 21:04:14 +0900

    [Silver III] Title: N과 M (8), Time: 284 ms, Memory: 20448 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main{
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] result;
+    public static int[] arr;
+    public static int N;
+    public static int M;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        result = new int[M];
+        for(int i = 0; i<N; i++)
+            arr[i] = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        backtracking(0,0);
+        System.out.println(sb);
+    }
+    public static void backtracking(int at, int count){
+        if(count == M){
+            for(int i : result)
+                sb.append(i).append(" ");
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = at; i < N; i++){
+            result[count] = arr[i];
+            backtracking(i,count+1);
+        }
+    }
+}
