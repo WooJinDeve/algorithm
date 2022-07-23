22a13b34d76ae4ad5802714e38d713d4d35c89cc
WooJinDeve <106054507+WooJinDeve@users.noreply.github.com>
2022-07-23 22:10:04 +0900

    [Silver II] Title: N과 M (11), Time: 1804 ms, Memory: 206364 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main{
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] result;
+    public static int[] arr;
+    public static int N;
+    public static int M;
+    public static HashSet<String> hashSet = new HashSet<>();
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        result = new int[M];
+        for(int i = 0; i<N; i++)
+            arr[i] = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        backtracking(0);
+        System.out.println(sb);
+    }
+    public static void backtracking(int count){
+        if(count == M){
+            StringBuilder temp = new StringBuilder();
+            for(int i : result)
+                temp.append(i).append(" ");
+            temp.append('\n');
+            if(!hashSet.contains(temp.toString())){
+                sb.append(temp);
+                hashSet.add(temp.toString());
+            }
+            return;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            result[count] = arr[i];
+            backtracking(count+1);
+        }
+    }
+}
