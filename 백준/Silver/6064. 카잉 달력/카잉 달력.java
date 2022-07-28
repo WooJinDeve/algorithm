9a7d0343937d792e05acd03f2debac43121247a7
WooJinDeve <106054507+WooJinDeve@users.noreply.github.com>
2022-07-28 10:49:19 +0900

    [Silver I] Title: 카잉 달력, Time: 288 ms, Memory: 17016 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.valueOf(bf.readLine());
+        while (t-- > 0) {
+            String[] line = bf.readLine().split(" ");
+            int m = Integer.valueOf(line[0]);
+            int n = Integer.valueOf(line[1]);
+            int x = Integer.valueOf(line[2])-1;
+            int y = Integer.valueOf(line[3])-1;
+            boolean ok = false;
+            for (int k=x; k<(n*m); k+=m) {
+                if (k%n == y) {
+                    System.out.println(k+1);
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                System.out.println(-1);
+            }
+        }
+    }
+}
