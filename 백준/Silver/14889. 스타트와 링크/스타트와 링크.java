1de0065925311c12d579d67664ff046273f348c1
WooJinDeve <106054507+WooJinDeve@users.noreply.github.com>
2022-07-28 20:30:37 +0900

    [Silver II] Title: 스타트와 링크, Time: 476 ms, Memory: 21860 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,49 +1,89 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    static int[][] s;
-    static int n;
-    static int go(int index, ArrayList<Integer> first, ArrayList<Integer> second) {
-        if (index == n) {
-            if (first.size() != n/2) return -1;
-            if (second.size() != n/2) return -1;
-            int t1 = 0;
-            int t2 = 0;
-            for (int i=0; i<n/2; i++) {
-                for (int j=0; j<n/2; j++) {
-                    if (i == j) continue;
-                    t1 += s[first.get(i)][first.get(j)];
-                    t2 += s[second.get(i)][second.get(j)];
+
+    static int N;
+    static int[][] map;
+    static boolean[] visit;
+
+    static int Min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+
+        map = new int[N][N];
+        visit = new boolean[N];
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = in.nextInt();
+            }
+        }
+
+        combi(0, 0);
+        System.out.println(Min);
+
+    }
+
+    // idx는 인덱스, count는 조합 개수(=재귀 깊이)
+    static void combi(int idx, int count) {
+        // 팀 조합이 완성될 경우
+        if(count == N / 2) {
+			/*
+			 방문한 팀과 방문하지 않은 팀을 각각 나누어
+			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
+			*/
+            diff();
+            return;
+        }
+
+        for(int i = idx; i < N; i++) {
+            // 방문하지 않았다면?
+            if(!visit[i]) {
+                visit[i] = true;	// 방문으로 변경
+                combi(i + 1, count + 1);	// 재귀 호출
+                visit[i] = false;	// 재귀가 끝나면 비방문으로 변경
+            }
+        }
+    }
+
+    // 두 팀의 능력치 차이를 계산하는 함수
+    static void diff() {
+        int team_start = 0;
+        int team_link = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                // i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스
+                if (visit[i] == true && visit[j] == true) {
+                    team_start += map[i][j];
+                    team_start += map[j][i];
+                }
+                // i 번째 사람과 j 번째 사람이 false라면 링크팀으로 점수 플러스
+                else if (visit[i] == false && visit[j] == false) {
+                    team_link += map[i][j];
+                    team_link += map[j][i];
                 }
             }
-            int diff = Math.abs(t1-t2);
-            return diff;
         }
-        int ans = -1;
-        first.add(index);
-        int t1 = go(index+1, first, second);
-        if (ans == -1 || (t1 != -1 && ans > t1)) {
-            ans = t1;
+        // 두 팀의 점수 차이 (절댓값)
+        int val = Math.abs(team_start - team_link);
+
+        /*
+         * 두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
+         * 더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
+         */
+        if (val == 0) {
+            System.out.println(val);
+            System.exit(0);
         }
-        first.remove(first.size()-1);
-        second.add(index);
-        int t2 = go(index+1, first, second);
-        if (ans == -1 || (t2 != -1 && ans > t2)) {
-            ans = t2;
-        }
-        second.remove(second.size()-1);
-        return ans;
+
+        Min = Math.min(val, Min);
+
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        s = new int[n][n];
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                s[i][j] = sc.nextInt();
-            }
-        }
-        ArrayList<Integer> first = new ArrayList<>();
-        ArrayList<Integer> second = new ArrayList<>();
-        System.out.println(go(0, first, second));
-    }
+
 }
