a55a2f02dcb2b7af859d396ae66f2ae43a8dce9d
WooJinDeve <106054507+WooJinDeve@users.noreply.github.com>
2022-07-30 20:09:47 +0900

    [Silver III] Title: 이친수, Time: 228 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] d = new long[n+1];
+        d[1] = 1;
+        if (n >= 2) {
+            d[2] = 1;
+        }
+        for (int i=3; i<=n; i++) {
+            d[i] = d[i-1] + d[i-2];
+        }
+        System.out.println(d[n]);
+    }
+}
