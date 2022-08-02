dd500ac01273bfc490209083d6dbb040cb325226
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-08-02 10:16:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 416 ms, Memory: 73552 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -10,28 +10,23 @@ public class Main {
         int[] d = new int[MAX];
         c[n] = true;
         d[n] = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        Queue<Integer> next_queue = new LinkedList<Integer>();
+        ArrayDeque<Integer> q = new ArrayDeque<Integer>();
         q.add(n);
         while (!q.isEmpty()) {
-            int now = q.remove();
+            int now = q.poll();
             for (int next : new int[]{now*2, now-1, now+1}) {
                 if (next >= 0 && next < MAX) {
                     if (c[next] == false) {
                         c[next] = true;
-                        if (now*2 == next) {
-                            q.add(next);
+                        if (next == now*2) {
+                            q.addFirst(next);
                             d[next] = d[now];
                         } else {
-                            next_queue.add(next);
+                            q.addLast(next);
                             d[next] = d[now] + 1;
-                        }                    
+                        }
                     }
                 }  
-            }
-            if (q.isEmpty()) {
-                q = next_queue;
-                next_queue = new LinkedList<Integer>();
             }
         }
         System.out.println(d[m]);
