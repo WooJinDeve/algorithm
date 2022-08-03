21ae4e6c1a1c51282c89113bc78577b08efe1855
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-08-03 19:40:36 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 388 ms, Memory: 29140 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
@@ -1,33 +1,27 @@
 import java.util.*;
 public class Main {
-    final static int[] dx = {0,0,0,0,1,-1};
-    final static int[] dy = {0,0,1,-1,0,0};
-    final static int[] dz = {1,-1,0,0,0,0};
+    final static int[] dx = {0,0,1,-1};
+    final static int[] dy = {1,-1,0,0};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] a = new int[102][102];
-        boolean[][][] d = new boolean[102][102][102];
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
                 a[i][j] = sc.nextInt();
-                for (int k=1; k<=a[i][j]; k++) {
-                    d[i][j][k] = true;
-                }
             }
         }
-        int ans = 0;
+        int ans = 2*n*m;
         for (int x=1; x<=n; x++) {
             for (int y=1; y<=m; y++) {
-                for (int z=1; z<=a[x][y]; z++) {
-                    for (int k=0; k<6; k++) {
-                        int nx = x+dx[k];
-                        int ny = y+dy[k];
-                        int nz = z+dz[k];
-                        if (d[nx][ny][nz] == false) ans += 1;
+                for (int k=0; k<4; k++) {
+                    int nx = x+dx[k];
+                    int ny = y+dy[k];
+                    if (a[x][y]-a[nx][ny] >= 0) {
+                        ans += a[x][y]-a[nx][ny];
                     }
-                }
+                    }
             }
         }
         System.out.println(ans);
