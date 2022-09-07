25a96d6d57aa57ba059f18e7bff3d8536874a991
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-07 18:29:34 +0900

    [Silver IV] Title: 조합, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/README.md
@@ -0,0 +1,24 @@
+# [Silver IV] 조합 - 2407 
+
+[문제 링크](https://www.acmicpc.net/problem/2407) 
+
+### 성능 요약
+
+메모리: 14288 KB, 시간: 124 ms
+
+### 분류
+
+임의 정밀도 / 큰 수 연산(arbitrary_precision), 조합론(combinatorics), 수학(math)
+
+### 문제 설명
+
+<p><sub>n</sub>C<sub>m</sub>을 출력한다.</p>
+
+### 입력 
+
+ <p>n과 m이 주어진다. (5 ≤ n ≤ 100, 5 ≤ m ≤ 100, m ≤ n)</p>
+
+### 출력 
+
+ <p><sub>n</sub>C<sub>m</sub>을 출력한다.</p>
+
---
+++ 백준/Silver/2407. 조합/조합.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        BigInteger sum = BigInteger.ONE;
+        BigInteger div = BigInteger.ONE;
+
+        for(int i = 0; i<m; i++) {
+            sum = sum.multiply(new BigInteger(String.valueOf(n-i)));
+            div = div.multiply(new BigInteger(String.valueOf(i+1)));
+        }
+
+        System.out.println(sum.divide(div));
+    }
+}
