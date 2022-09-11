b7ffc6e775b2c7aac8ae4e41b833eb95d8c83474
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-11 21:53:53 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 304 ms, Memory: 20724 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,25 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n+1];
         int[] d = new int[n+1];
-        for (int i=1; i<=n; i++) {
+        for (int i=0; i<n; i++) {
             a[i] = sc.nextInt();
-        }
-        for (int i=n; i>=1; i--) {
             d[i] = 1;
-            for (int j=i+1; j<=n; j++) {
-                if (a[i] > a[j] && d[i] < d[j]+1) {
-                    d[i] = d[j]+1;
-                }
-            }
         }
-        int ans = d[1];
-        for (int i=2; i<=n; i++) {
-            if (ans < d[i]) {
-                ans = d[i];
+
+        int ans = d[0];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                if (a[j] > a[i] && d[i] < d[j]+1) {
+                    d[i] = d[j]+1;
+                    ans = Math.max(d[i], ans);
+                }
             }
         }
         System.out.println(ans);
