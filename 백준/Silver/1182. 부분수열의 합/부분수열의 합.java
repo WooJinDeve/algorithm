c79ad51e69886e843a4591f80cb40e23785e520a
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-25 23:16:47 +0900

    [Silver II] Title: 부분수열의 합, Time: 220 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,25 +1,26 @@
 import java.util.*;
-
 public class Main {
+    public static int go(int[] a, int m, int i, int sum) {
+        if (i == a.length) {
+            if (sum == m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        return go(a, m, i+1, sum+a[i]) + go(a, m, i+1, sum);
+    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int s = sc.nextInt();
+        int m = sc.nextInt();
         int[] a = new int[n];
         for (int i=0; i<n; i++) {
             a[i] = sc.nextInt();
         }
-        int ans = 0;
-        for (int i=1; i<(1<<n); i++) {
-            int sum = 0;
-            for (int k=0; k<n; k++) {
-                if ((i&(1<<k)) != 0) {
-                    sum += a[k];
-                }
-            }
-            if (sum == s) {
-                ans += 1;
-            }
+        int ans = go(a, m, 0, 0);
+        if (m == 0) {
+            ans -= 1;
         }
         System.out.println(ans);
     }
