1a559224374c5513f12a4692bb3738b2f57eb819
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-27 10:44:13 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 432 ms, Memory: 83804 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        int n = a.length();
+        int m = b.length();
+        a = " " + a;
+        b = " " + b;
+        int[][] d = new int[n+1][m+1];
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    d[i][j] = d[i-1][j-1] + 1;
+                } else {
+                    d[i][j] = 0;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (ans < d[i][j]) {
+                    ans = d[i][j];
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
