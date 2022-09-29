31f0e04645a0d734567f72911da6f030f6d6ff70
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-29 21:19:17 +0900

    [Gold IV] Title: N-Queen, Time: 2880 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,37 +1,49 @@
 import java.util.*;
 public class Main {
-    static int N = 0;
-    static int[] vy = new int[16];
-    static int[] vx = new int[16];
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        int r = 0;
-        for(int i = 0; i<N; i++)
-            r += go(0,i);
-
-        System.out.println(r);
-    }
-    public static int go(int y, int x){
-        for(int i = 0; i<y; i++){
-            if(y==vy[i]) return  0;
-            if(x==vx[i]) return  0; //
-            if(Math.abs(x-vx[i]) == Math.abs(y-vy[i])) return  0; // 대각선
+    static boolean[][] a = new boolean[15][15];
+    static int n;
+    static boolean[] check_col = new boolean[15];
+    static boolean[] check_dig = new boolean[40];
+    static boolean[] check_dig2 = new boolean[40];
+    static boolean check(int row, int col) {
+        // |
+        if (check_col[col]) {
+            return false;
         }
-
-        if(y == N - 1){
+        // 왼쪽 위 대각선
+        if (check_dig[row+col]) {
+            return false;
+        }
+        // /
+        if (check_dig2[row-col+n]) {
+            return false;
+        }
+        return true;
+    }
+    static int calc(int row) {
+        if (row == n) {
+            // ans += 1;
             return 1;
         }
-        vy[y] = y;
-        vx[y] = x;
-
-        int r = 0;
-        for (int i = 0; i<N; i++){
-            r+= go(y+1,i);
+        int cnt = 0;
+        for (int col=0; col<n; col++) {
+            if (check(row, col)) {
+                check_dig[row+col] = true;
+                check_dig2[row-col+n] = true;
+                check_col[col] = true;
+                a[row][col] = true;
+                cnt += calc(row+1);
+                check_dig[row+col] = false;
+                check_dig2[row-col+n] = false;
+                check_col[col] = false;
+                a[row][col] = false;
+            }
         }
-        return r;
-
+        return cnt;
     }
-
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(calc(0));
+    }
 }
