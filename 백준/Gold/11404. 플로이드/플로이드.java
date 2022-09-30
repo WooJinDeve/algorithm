be29e4967c6a423285d0bae95cdc06e90b1aa238
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-09-30 09:24:19 +0900

    [Gold IV] Title: 플로이드, Time: 472 ms, Memory: 45980 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    static final int INF = 987654321;
+ 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+ 
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N + 1][N + 1];
+ 
+        // 초기값 설정
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = INF;
+ 
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+ 
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            // 출발 도시와 도착 도시가 같지만 시간이 다른 입력값이 들어올 수 있음.
+            // 예를 들어 "1 4 1"과 "1 4 2"가 입력으로 들어왔으면,
+            // "1 4 1"을 택해야 함.
+            arr[a][b] = Math.min(arr[a][b], c); 
+        }
+ 
+        // 플로이드 와샬 알고리즘
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    // 최단경로 초기화
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+ 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                // 갈 수 없는 곳은 0으로 초기화
+                if (arr[i][j] == INF) {
+                    arr[i][j] = 0;
+                }
+ 
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+ 
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+ 
+}
