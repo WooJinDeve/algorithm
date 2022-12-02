0593712e37ba862848f78f631120e03f7bde6dea
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-12-02 20:30:08 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,25 +1,14 @@
-import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        List<Integer> yellows = measure(yellow);
-        for (int i = 0; i < yellows.size() - 1; i++) {
-            int row = yellows.get(i + 1) + 2;
-            int colum = yellows.get(i) + 2;
-            if (brown == (row - 2) * 2 + (colum * 2))
-                return new int[]{row, colum};
-        }
-        return null;
-    }
-    
-        public static List<Integer> measure(int x){
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 1; i <= Math.sqrt(x); i++) {
-            if (x % i == 0){
-                arr.add(i);
-                arr.add(x / i);
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            if (yellow % i == 0){
+                int row = yellow / i + 2;
+                int colum = i + 2;
+                if (brown == (row - 2) * 2 + (colum * 2))
+                    return new int[]{row, colum};
             }
         }
-        return arr;
+        return null;
     }
 }
