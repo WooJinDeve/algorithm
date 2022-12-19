f6591fe386cc04315280423640775eaa64619cc7
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-12-19 20:03:04 +0900

    [level 2] Title: 땅따먹기, Time: 19.79 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] land){
+        int ans = 0;
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (ans < land[land.length - 1][i])
+                ans = land[land.length - 1][i];
+        }
+        return ans;
+    }
+
+}
