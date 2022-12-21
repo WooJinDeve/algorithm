40f00bc00d522d1a094c62ba9531083893e14eb4
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-12-21 20:37:04 +0900

    [level 2] Title: 위장, Time: 0.63 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+        }
+        return map.values().stream()
+                .reduce(1, (a, b) -> a * b) - 1;
+    }
+}
