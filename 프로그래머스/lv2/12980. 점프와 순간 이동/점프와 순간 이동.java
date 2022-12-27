d3ba5ee87757a494713f69f0137322288361555b
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2022-12-27 20:24:52 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n != 0){
+            if(n % 2 == 0){
+                n /= 2;
+            } else {
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
