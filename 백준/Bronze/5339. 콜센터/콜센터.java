09ec23b10c3f7c12ccaebd77ca48ec5c6d66ff35
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2023-01-09 09:39:12 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 13828 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
