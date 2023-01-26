5f279d8c530abd10dbddca319d0ea4cc19bc5192
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2023-01-26 18:11:39 +0900

    [Silver IV] Title: 기타줄, Time: 236 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M =sc.nextInt();
+        int Min = Integer.MAX_VALUE;
+ 
+        int[] unit = new int[M];
+        int[] pack = new int[M];
+        for(int i=0; i<M; i++){
+            pack[i] = sc.nextInt();
+            unit[i] = sc.nextInt();
+        }
+        Arrays.sort(unit);
+        Arrays.sort(pack);
+        
+        Min = Math.min(((N/6)+1)*pack[0], N*unit[0]);    
+        Min = Math.min(Min, ((N/6))*pack[0] + (N%6)*unit[0]);
+        
+        System.out.println(Min);
+    }
+}
