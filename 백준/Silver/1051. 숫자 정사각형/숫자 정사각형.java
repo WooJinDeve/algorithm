bd7ece8b1d3bea9ef9d30f8b88cdbfdc64f3e238
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2023-01-30 15:24:44 +0900

    [Silver IV] Title: 숫자 정사각형, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    static int N,M, ans=Integer.MIN_VALUE;
+    static int[][] map;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for(int i=0; i<N; i++) {
+            String str = br.readLine();
+            for(int j=0; j<M; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+        
+        int len = Math.min(N, M);
+        
+        while(len > 1) {
+            for(int i=0; i<= N-len; i++) {
+                for(int j=0; j<=M-len; j++) {
+                    int num = map[i][j];
+                    // 꼭짓점 3군데 비교
+                    if(num==map[i][j+len-1] && num == map[i+len-1][j] && num == map[i+len-1][j+len-1]) {
+                        System.out.println(len*len);
+                        return;
+                    }
+                }
+            }
+            len--;
+        }
+        
+        System.out.println(len*len);
+    } 
+}
