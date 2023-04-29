5578e108c46b330c9f8d8a2b153df8f5d768cdba
정우진 <106054507+WooJinDeve@users.noreply.github.com>
2023-04-30 00:49:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,38 +1,12 @@
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        List<String> bit1 = convertToBinaryString(n, arr1);
-        List<String> bit2 = convertToBinaryString(n, arr2);
-
-        String[] answer = new String[n];
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (bit1.get(i).charAt(j) == '1' || bit2.get(i).charAt(j) == '1') {
-                    sb.append("#");
-                    continue;
-                }
-                sb.append(" ");
-            }
-            answer[i] = sb.toString();
-        }
-
-        return answer;
-    }
-
-    private List<String> convertToBinaryString(int n, int[] arr) {
-        return Arrays.stream(arr)
-                .mapToObj(Integer::toBinaryString)
-                .map(binary -> addEmptyBinaryString(binary, n))
-                .collect(Collectors.toList());
-    }
-
-    private String addEmptyBinaryString(String binary, int n){
-        if (binary.length() == n) {
-            return binary;
-        }
-        return "0".repeat(n - binary.length()) + binary;
+        return IntStream.range(0, n)
+                .mapToObj(i -> Integer.toBinaryString(arr1[i] | arr2[i]))
+                .map(binary -> String.format("%" + n + "s", binary))
+                .map(binary -> binary.replaceAll("1", "#").replaceAll("0", " "))
+                .toArray(String[]::new);
     }
 }
